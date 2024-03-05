5d6270f007ca8e6a335cb3fee4515b2f87f30d6a
Abass Ibrahim <abassdibrahim@gmail.com>
2024-03-05 23:50:27 +0000

    added donut chart

--- timesage/src/components/topbar.js
+++ timesage/src/components/topbar.js
@@ -6,7 +6,7 @@ import GetUsername from "./getUsername";
 const Topbar = () => {
   return (
     <>
-      <div className="gap-4 px-4 w-full py-10 h-16 sticky top-0 items-center justify-end flex bg-background border-b border-line z-10">
+      <div className="gap-4 px-10 w-full py-4 sticky top-0 items-center justify-end flex bg-background border-b border-line z-10">
         <GetUsername />
         <ThemeSwitcher />
         <SignOut />
